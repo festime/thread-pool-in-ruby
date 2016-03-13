c61fe352cf53c9ab3bd8b2430f51268ebbc3d4d4
Yulin Chen <gloverock2@gmail.com>
2016-03-13 11:19:58 +0800

    Write a test about ThreadPool size and find its size will bloat when continuously pushing jobs into it.

--- test_thread_pool.rb
+++ test_thread_pool.rb
@@ -63,6 +63,42 @@ class TestThreadPool < Minitest::Test
       'Elapsed time was too long: %.1f seconds' % elapsed
   end
 
+  # 這個測項是測 ThreadPool 不會因為外部要求的工作
+  # 而一直多開 threads 出來
+  # 避免 threads 數量無謂的或超乎預期的膨脹
+  # 造成記憶體消耗過大或
+  # 作業系統要在很多 threads 之前切換的問題
+  #
+  # 因為這時 ThreadPool 的 implementation 還沒改
+  # 跑這個測項有點危險， threads 數量會一直增加
+  # 造成作業系統要在過多 threads 之間切換
+  # 會讓電腦沒有回應...
+  def test_pool_size_limit
+    pool_size = 5
+    pool = ThreadPool.new(size: pool_size)
+    mutex = Mutex.new
+    # Ruby 的 Set 是標準函式庫，需要自己 require
+    # 它提供容器用於裝沒有順序且無重複的值
+    # 例如 1 已經在一個 Set instance 裡面
+    # 那再塞 1 進去不會有任何改變
+    # Set instance 內容物維持原樣
+    threads = Set.new
+
+    100_000.times do
+      pool.schedule do
+        mutex.synchronize do
+          # 如果一個 Thread 物件已經在 threads
+          # 這個 Set instance 裡面
+          # 那再塞進去其實不會改變內容任何東西
+          threads << Thread.current
+        end
+      end
+    end
+    pool.shutdown
+
+    assert_equal(pool_size, threads.size)
+  end
+
   private
 
   # 自己寫一個 helper
