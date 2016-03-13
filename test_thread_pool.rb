cd119a3e9139e9562250d3dbceb5ca83dc6ea155
Yulin Chen <gloverock2@gmail.com>
2016-03-13 10:20:42 +0800

    Add comments in Chinese.

--- test_thread_pool.rb
+++ test_thread_pool.rb
@@ -7,6 +7,13 @@ class TestThreadPool < Minitest::Test
     pool_size = 5
     pool = ThreadPool.new(size: pool_size)
 
+    # 用於鎖住一段 code
+    # 讓這段 code 同一時間只能被一個 thread 執行
+    # 避免多個 threads 同時對一個物件操作
+    # 造成資料讀寫錯誤
+    # 但是 Ruby MRI 有 Global Interpreter Lock
+    # 已經保證同一時間一段 code 只能被一個 thread 執行
+    # 所以這裡沒用 mutex 在 Ruby MRI 應該無所謂...
     mutex = Mutex.new
 
     iterations = pool_size * 3
@@ -14,6 +21,8 @@ class TestThreadPool < Minitest::Test
 
     iterations.times do |i|
       pool.schedule do
+        # 鎖住這段 code
+        # 確保同一時間只能有一個 thread 執行
         mutex.synchronize do
           results[i] = i + 1
         end
@@ -21,6 +30,10 @@ class TestThreadPool < Minitest::Test
     end
     pool.shutdown
 
+    # 斷言執行結果應該是 [1, 2, ..., 14, 15]
+    # 換言之
+    # 預期塞數字進 results
+    # 應該照數字從小到大的順序跑
     assert_equal(1.upto(pool_size * 3).to_a, results)
   end
 end
--- thread_pool.rb
+++ thread_pool.rb
@@ -1,11 +1,18 @@
+# TDD 精神之一就是做最小的努力去讓 failed tests 通過
+# 所以寫一個 ThreadPool class
 class ThreadPool
+  # 初始化要能接受 { size: }
   def initialize(size:)
   end
 
+  # ThreadPool instance 要能執行外部呼叫 schedule 時
+  # 夾帶的 block 裡面的程式碼
   def schedule(*args, &block)
     block.call(args)
   end
 
+  # ThreadPool instance 要能回應 shutdown
+  # 即使裡面其實什麼事也沒做
   def shutdown
   end
 end
